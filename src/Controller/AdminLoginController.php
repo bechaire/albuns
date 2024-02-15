5ed3f8f57ba43de010d7c2d5ddd16a4990220241
bechaire <bechaire@gmail.com>
2024-02-14 21:36:22 -0300

    Conclusão da área de logon, com validação de usuário e senha no LDAP

--- src/Controller/AdminLoginController.php
+++ src/Controller/AdminLoginController.php
@@ -5,14 +5,13 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Repository\UsuarioRepository;
-use App\Service\LdapUserValidationService;
+use App\Service\UserValidationService;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class AdminLoginController extends AbstractController
 {
@@ -26,55 +25,15 @@ class AdminLoginController extends AbstractController
     #[Route('/admin/login', name: 'app_admin_login', methods: ['GET'])]
     public function loginForm(): Response
     {
+        // $userLogged = $this->security->getUser();
+        if ($this->container->get('security.authorization_checker')->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('app_admin_home', status: Response::HTTP_SEE_OTHER);
+        }
+
         return $this->render('admin_area/login.html.twig');
     }
 
-    #[Route('/admin/login', name: 'app_admin_login_post', methods: ['POST'])]
-    public function loginValidation(Request $request, LdapUserValidationService $ldap): Response
-    {
-        $isDevMode = strtolower($this->getParameter('kernel.environment')) == 'dev';
-
-        if (!$isDevMode) {
-            sleep(rand(1,3));
-        }
-
-        $usuarioInformado = $request->request->get('usuario');
-        $senhaInformada = $request->request->get('senha');
-
-        $usuarioOuSenhaEmBranco = empty($usuarioInformado) || empty($senhaInformada);
-
-        if ($usuarioOuSenhaEmBranco) {
-            $this->addFlash('warning text-center', 'Usuário ou senha inválidos');
-            return $this->redirectToRoute('app_admin_login', status: Response::HTTP_SEE_OTHER);
-        }
-
-        $usuario = $this->usuarioRepository->findOneBy([
-            'usuario' => $request->request->get('usuario'),
-            'ativo' => 'S'
-        ]);
-
-        if (!$usuario) {
-            $this->addFlash('warning text-center', 'Usuário ou senha inválidos');
-            $this->log->warning('Tentativa de logon com usuário não cadastrado previamente ou desativado', [$request->request->get('usuario')]);
-            return $this->redirectToRoute('app_admin_login', status: Response::HTTP_SEE_OTHER);
-        }
-
-        $usuarioLocalizado = $usuario->getUsuario();
-        $usuarioValido = $ldap->isValid($usuarioLocalizado, $senhaInformada);
-
-        if (!$usuarioValido) {
-            $this->addFlash('warning text-center', 'Usuário ou senha inválidos');
-            return $this->redirectToRoute('app_admin_login', status: Response::HTTP_SEE_OTHER);
-        }
-
-        $redirectResponse = $this->security->login($usuario, 'form_login');
-
-        // return $this->redirectToRoute('app_admin', status: Response::HTTP_SEE_OTHER);
-        return $redirectResponse;
-
-    }
-
-    #[Route('/admin/logout', name: 'app_admin_logout')]
+    #[Route('/admin/logout', name: 'app_admin_logout', methods: ['GET'])]
     public function adminLogout(): Response
     {
         $this->security->logout(false);
@@ -82,23 +41,32 @@ class AdminLoginController extends AbstractController
         return $this->redirectToRoute('app_admin_login', status: Response::HTTP_SEE_OTHER);
     }
 
-    #[Route('/admin', name: 'app_admin')]
-    public function adminArea(): Response
+    #[Route('/admin/login', name: 'app_admin_login_post', methods: ['POST'])]
+    public function loginValidation(Request $request, UserValidationService $validation): Response
     {
-        return new Response('<body>Acessou /admin</body>');
+        $isProdMode = strtolower($this->getParameter('kernel.environment')) == 'prod';
+
+        if ($isProdMode) {
+            sleep(rand(1,3));
+        }
+
+        $usuarioInformado = $request->request->get('usuario');
+        $senhaInformada = $request->request->get('senha');
+
+        try {
+            $usuario = $validation->validateUserPassword($usuarioInformado, $senhaInformada);
+        } catch(\DomainException) {
+            $this->addFlash('warning text-center', 'Usuário ou senha inválidos');
+            return $this->redirectToRoute('app_admin_login', status: Response::HTTP_SEE_OTHER);
+        }
+
+        $usuario->setUltacesso(new \DateTimeImmutable());
+        $this->usuarioRepository->add($usuario, true);
+
+        $redirectResponse = $this->security->login($usuario, 'form_login');
+
+        return $redirectResponse;
+
     }
 
-    #[IsGranted('ROLE_ADMIN')]
-    #[Route('/admin/usuarios', name: 'app_admin_usuarios', methods: ['GET'])]
-    public function adminUsersList(): Response
-    {
-        return new Response('<body>Acessou /admin/usuarios via GET</body>');
-    }
-
-    #[IsGranted('ROLE_ADMIN')]
-    #[Route('/admin/usuarios', name: 'app_admin_usuarios_post', methods: ['POST'])]
-    public function adminUsersAdd(): Response
-    {
-        return new Response('<body>Acessou /admin/usuarios via POST</body>');
-    }
 }
--- src/Service/UserValidationService.php
+++ src/Service/UserValidationService.php
@@ -4,21 +4,55 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Entity\Usuario;
+use App\Repository\UsuarioRepository;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Ldap\Ldap;
 use Symfony\Component\Ldap\Exception\ConnectionException;
 use Symfony\Component\Ldap\Exception\InvalidCredentialsException;
 
-class LdapUserValidationService
+class UserValidationService
 {
     public function __construct(
         private ParameterBagInterface $params,
-        private LoggerInterface $log
+        private LoggerInterface $log,
+        private UsuarioRepository $usuarioRepository
     ) {
     }
 
-    public function isValid(string $ldapUser, #[\SensitiveParameter] string $ldapRawPass): bool
+    public function validateUserPassword(?string $user, #[\SensitiveParameter] ?string $password): Usuario
+    {
+        $usuarioOuSenhaEmBranco = empty($user) || empty($password);
+
+        if ($usuarioOuSenhaEmBranco) {
+            throw new \DomainException('Usuário ou senha inválidos');
+        }
+
+        $usuario = $this->usuarioRepository->findOneBy([
+            'usuario' => $user,
+            'ativo' => 'S'
+        ]);
+
+        if (!$usuario) {
+            $this->log->warning('Tentativa de logon com usuário não cadastrado previamente ou desativado', [$user]);
+            throw new \DomainException('Usuário ou senha inválidos');
+        }
+
+        $usuarioLocalizado = $usuario->getUsuario();
+        $usuarioValido = $this->validateLDAPPassword($usuarioLocalizado, $password);
+
+        if (!$usuarioValido) {
+            $this->log->warning('Tentativa de logon com usuário localizado, mas senha incorreta', [$user]);
+            throw new \DomainException('Usuário ou senha inválidos');
+        }
+
+        $this->log->info('Sucesso ao validar usuário e senha', $this->userAccessInfo($user));
+
+        return $usuario;
+    }
+
+    public function validateLDAPPassword(string $ldapUser, #[\SensitiveParameter] string $ldapRawPass): bool
     {
         $ldap = Ldap::create('ext_ldap', [
             'host' => $this->params->get('app.ldap.host')
@@ -28,25 +62,22 @@ class LdapUserValidationService
             $ldap->bind($this->params->get('app.ldap.usrprefix') . $ldapUser, $ldapRawPass);
             return true;
         } catch (InvalidCredentialsException) {
-            $this->log->warning('Falha ao validar a senha no LDAP', [
-                'Usuário' => $ldapUser,
-                'IP' => $_SERVER['REMOTE_ADDR'],
-                'Path' => $_SERVER['REQUEST_URI'],
-            ]);
+            $this->log->warning('Falha ao validar a senha no LDAP', $this->userAccessInfo($ldapUser));
         } catch (ConnectionException) {
-            $this->log->critical('Erro de conexão ao LDAP', [
-                'Usuário' => $ldapUser,
-                'IP' => $_SERVER['REMOTE_ADDR'],
-                'Path' => $_SERVER['REQUEST_URI'],
-            ]);
+            $this->log->critical('Erro de conexão ao LDAP', $this->userAccessInfo($ldapUser));
         } catch (\Exception $e) {
-            $this->log->alert('Erro desconhecido ao validar Usuário e Senha no LDAP', [
-                'Usuário' => $ldapUser,
-                'IP' => $_SERVER['REMOTE_ADDR'],
-                'Path' => $_SERVER['REQUEST_URI'],
-            ]);
+            $this->log->alert('Erro desconhecido ao validar Usuário e Senha no LDAP', $this->userAccessInfo($ldapUser));
         }
 
         return false;
+    }
+
+    private function userAccessInfo(string $username): array
+    {
+        return [
+            'Usuário' => $username,
+            'IP' => $_SERVER['REMOTE_ADDR'] ?? null,
+            'Path' => $_SERVER['REQUEST_URI'] ?? null,
+        ];
     }
 }
