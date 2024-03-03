74c41d92a43863f54b4f16b6d1e5f6756c6d7b32
bechaire <bechaire@gmail.com>
2024-03-03 19:47:52 -0300

    Separado controller público de administrativo/privado

--- src/Controller/AdminFotoController.php
+++ src/Controller/AdminFotoController.php
@@ -15,7 +15,6 @@ use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Filesystem\Exception\IOException;
 use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
@@ -34,17 +33,6 @@ class AdminFotoController extends AbstractController
         private LoggerInterface $log,
         private EntityManagerInterface $entityManager,
     ) {
-    }
-
-    #[Route('/images/cache/{idalbum}/{tamanho}/{arquivo}', name: 'app_exibe_imagem', methods: ['GET'])]
-    public function exibeImagem(int $idalbum, string $tamanho, string $arquivo, Request $request): Response
-    {
-        $pathArquivoCriado = $this->cacheiaArquivoSolicitado($idalbum, $tamanho, $arquivo);
-        return new BinaryFileResponse(
-            file: $pathArquivoCriado,
-            autoEtag: true,
-            autoLastModified: true,
-        );
     }
 
     #[Route('/admin/albuns/{idalbum}/fotos', name: 'app_admin_albuns_fotos_list', methods: ['GET'])]
@@ -223,67 +211,4 @@ class AdminFotoController extends AbstractController
         $this->image->saveAsJpeg($compressaoJpeg, true);
     }
 
-    private function cacheiaArquivoSolicitado(int $idalbum, string $tamanho, string $arquivo): string
-    {
-        $identificador = str_ireplace(['.jpg', '.jpeg', '.jfif'], '', $arquivo);
-
-        $album = $this->entityManager->getReference(Album::class, $idalbum);
-
-        $foto = $this->fotoRepository->getInfoFoto($album, $identificador);
-        if (!$foto) {
-            throw $this->createNotFoundException('O arquivo solicitado não foi encontrado #1');
-        }
-
-        $arquivoArmazenado = $foto->getArquivo();
-        $pathArquivoOrigem = $this->getParameter('app.albuns.path') . "/{$idalbum}/{$arquivoArmazenado}";
-        if (!$this->filesystem->exists($pathArquivoOrigem)) {
-            throw $this->createNotFoundException('O arquivo solicitado não foi encontrado #2');
-        }
-
-        $tamanhosDisponiveis = $this->getParameter('app.albuns.cache.sizes');
-        $tamanhoPX = array_search($tamanho, $tamanhosDisponiveis);
-        if (!$tamanhoPX) {
-            throw $this->createNotFoundException('O arquivo solicitado não foi encontrado #3');
-        }
-
-        $pathDestino = $this->getParameter('app.albuns.cache.path') . "/{$idalbum}/{$tamanho}";
-        try {
-            $this->filesystem->mkdir($pathDestino, 0766);
-        } catch (IOException) {
-            $this->log->error('Não foi possível criar o diretório de destino', [$pathDestino]);
-            throw $this->createNotFoundException('O arquivo solicitado não pôde ser criado #1');
-        }
-
-        $pathArquivoDestino = $pathDestino . "/{$identificador}.jpg";
-
-        // pode haver solicitação de um arquivo a ser cacheado que foi criado a segundos antes
-        if ($this->filesystem->exists($pathArquivoDestino)) {
-            return $pathArquivoDestino;
-        }
-
-        $opcoesArray = json_decode($foto->getOpcoes(), true);
-
-        try {
-            $compressaoJpeg = 80;
-            $this->image->loadImage($pathArquivoOrigem);
-
-            if ($opcoesArray['fliph'] > 0) {
-                $this->image->flipH();
-            }
-            if ($opcoesArray['flipv'] > 0) {
-                $this->image->flipV();
-            }
-            if ($opcoesArray['rotate'] > 0) {
-                $this->image->rotate($opcoesArray['rotate']);
-            }
-
-            $this->image->thumb($tamanhoPX);
-            $this->image->saveAsJpeg(compress: $compressaoJpeg, newFilePath: $pathArquivoDestino);
-        } catch (ImageException) {
-            $this->log->error('Não foi possível criar o arquivo de destino', [$pathArquivoDestino]);
-            throw $this->createNotFoundException('O arquivo solicitado não pôde ser criado #2');
-        }
-
-        return $pathArquivoDestino;
-    }
 }
---
+++ src/Controller/AlbumController.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Album;
+use App\Exception\ImageException;
+use App\Interface\ImageFacadeInterface;
+use App\Repository\FotoRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Exception\IOException;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AlbumController extends AbstractController
+{
+    public function __construct(
+        private FotoRepository $fotoRepository,
+        private Filesystem $filesystem,
+        private ImageFacadeInterface $image,
+        private LoggerInterface $log,
+        private EntityManagerInterface $entityManager,
+    ) {
+    }
+
+    #[Route('/images/cache/{idalbum}/{tamanho}/{arquivo}', name: 'app_exibe_imagem', methods: ['GET'])]
+    public function exibeImagem(int $idalbum, string $tamanho, string $arquivo): Response
+    {
+        $pathArquivoCriado = $this->cacheiaArquivoSolicitado($idalbum, $tamanho, $arquivo);
+        return new BinaryFileResponse(
+            file: $pathArquivoCriado,
+            autoEtag: true,
+            autoLastModified: true,
+        );
+    }
+
+    private function cacheiaArquivoSolicitado(int $idalbum, string $tamanho, string $arquivo): string
+    {
+        $identificador = str_ireplace(['.jpg', '.jpeg', '.jfif'], '', $arquivo);
+
+        $album = $this->entityManager->getReference(Album::class, $idalbum);
+
+        $foto = $this->fotoRepository->getInfoFoto($album, $identificador);
+        if (!$foto) {
+            throw $this->createNotFoundException('O arquivo solicitado não foi encontrado #1');
+        }
+
+        $arquivoArmazenado = $foto->getArquivo();
+        $pathArquivoOrigem = $this->getParameter('app.albuns.path') . "/{$idalbum}/{$arquivoArmazenado}";
+        if (!$this->filesystem->exists($pathArquivoOrigem)) {
+            throw $this->createNotFoundException('O arquivo solicitado não foi encontrado #2');
+        }
+
+        $tamanhosDisponiveis = $this->getParameter('app.albuns.cache.sizes');
+        $tamanhoPX = array_search($tamanho, $tamanhosDisponiveis);
+        if (!$tamanhoPX) {
+            throw $this->createNotFoundException('O arquivo solicitado não foi encontrado #3');
+        }
+
+        $pathDestino = $this->getParameter('app.albuns.cache.path') . "/{$idalbum}/{$tamanho}";
+        try {
+            $this->filesystem->mkdir($pathDestino, 0766);
+        } catch (IOException) {
+            $this->log->error('Não foi possível criar o diretório de destino', [$pathDestino]);
+            throw $this->createNotFoundException('O arquivo solicitado não pôde ser criado #1');
+        }
+
+        $pathArquivoDestino = $pathDestino . "/{$identificador}.jpg";
+
+        // pode haver solicitação de um arquivo a ser cacheado que foi criado a segundos antes
+        if ($this->filesystem->exists($pathArquivoDestino)) {
+            return $pathArquivoDestino;
+        }
+
+        $opcoesArray = json_decode($foto->getOpcoes(), true);
+
+        try {
+            $compressaoJpeg = 80;
+            $this->image->loadImage($pathArquivoOrigem);
+
+            if ($opcoesArray['fliph'] > 0) {
+                $this->image->flipH();
+            }
+            if ($opcoesArray['flipv'] > 0) {
+                $this->image->flipV();
+            }
+            if ($opcoesArray['rotate'] > 0) {
+                $this->image->rotate($opcoesArray['rotate']);
+            }
+
+            $this->image->thumb($tamanhoPX);
+            $this->image->saveAsJpeg(compress: $compressaoJpeg, newFilePath: $pathArquivoDestino);
+        } catch (ImageException) {
+            $this->log->error('Não foi possível criar o arquivo de destino', [$pathArquivoDestino]);
+            throw $this->createNotFoundException('O arquivo solicitado não pôde ser criado #2');
+        }
+
+        return $pathArquivoDestino;
+    }
+}
